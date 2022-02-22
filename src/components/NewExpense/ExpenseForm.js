16c4b95353fdc683bfcfb5b88becda3abfd4f8c6
jr1412594 <jr.medina1412@gmail.com>
2022-02-22 13:04:17 -0700

    form state data

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,30 +1,71 @@
-
-import './ExpenseForm.css'
+import { useState } from "react";
+import "./ExpenseForm.css";
 
 const ExpenseForm = () => {
+
+    const [expense, setExpense] = useState({
+        title: "",
+        amount: "",
+        date: "",
+    });
+
+    const changeHandler = (event) => {
+        setExpense((prevState) => {
+            return { ...prevState, [event.target.name]: event.target.value };
+        });
+    };
     
+    const submitHandler = (event) => {
+        event.preventDefault();
+        let updatedExpense = {...expense, [expense.date]: new Date(expense.date) }
+        console.log(updatedExpense, " :this data has been submitted")
+        setExpense({
+            title: '',
+            amount: '',
+            date: ''
+        })
+    };
 
     return (
-        <form>
+        <form onSubmit={submitHandler}>
             <div className="new-expense_controls">
                 <div className="new-expense_control">
                     <label>Title: </label>
-                    <input type="text" />
+                    <input
+                        value={expense.title}
+                        type="text"
+                        name="title"
+                        onChange={changeHandler}
+                    />
                 </div>
                 <div className="new-expense_control">
                     <label>Amount: </label>
-                    <input type="number" min="0.01" step="0.01"/>
+                    <input
+                        type="number"
+                        value={expense.amount}
+                        name="amount"
+                        onChange={changeHandler}
+                        min="0.01"
+                        step="0.01"
+                    />
                 </div>
                 <div className="new-expense_control">
                     <label>Date: </label>
-                    <input type="date" min="2019-01-01" max="2022-12-31"/>
+                    <input
+                        type="date"
+                        name="date"
+                        value={expense.date}
+                        onChange={changeHandler}
+                        min="2019-01-01"
+                        max="2022-12-31"
+                    />
                 </div>
             </div>
             <div className="new-expense_actions">
                 <button type="submit">Add Expense</button>
             </div>
         </form>
-    )
-}
+    );
+};
 
 export default ExpenseForm;
