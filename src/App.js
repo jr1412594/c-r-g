59d35428f917a5a8a1cf801853d85873710a1ffa
jr1412594 <jr.medina1412@gmail.com>
2022-02-23 10:57:30 -0700

    extract initial state, App is stateful component

--- src/App.js
+++ src/App.js
@@ -1,54 +1,27 @@
+import { useState } from "react";
 import ExpensesContainer from "./components/Expenses/ExpensesContainer";
 import NewExpense from "./components/NewExpense/NexExpense";
 
-const App = () => {
+import { start as DUMMY_EXPENSES } from "./data/InitialState";
 
-    const expenses = [
-        {
-            id: "e1",
-            title: "Toilet Paper",
-            amount: 94.12,
-            date: new Date(2020, 7, 14),
-        },
-        {
-            id: "e2",
-            title: "New TV",
-            amount: 799.49,
-            date: new Date(2021, 2, 12),
-        },
-        {
-            id: "e3",
-            title: "Car Insurance",
-            amount: 294.67,
-            date: new Date(2021, 2, 28),
-        },
-        {
-            id: "e4",
-            title: "New Desk (Wooden)",
-            amount: 450,
-            date: new Date(2021, 5, 12),
-        },
-        {
-            id: "e5",
-            title: "Dog Cone",
-            amount: 11.99,
-            date: new Date(2019, 6, 6),
-        },
-    ];
+const App = () => {
+    const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
 
     const addExpense = (expenseToAdd) => {
-      console.log(expenseToAdd, " :from app")
-      return [...expenses, expenseToAdd];
-      // console.log(newExpenses, " :Expenses AD.")
-    }
+        console.log(expenseToAdd, " :from app");
+        setExpenses((prevState) => {
+            return [...prevState, expenseToAdd];
+        });
+        console.log(expenses, "in add Expense");
+    };
 
     return (
         <div>
-            <h2>Let's get started!</h2>
-            <NewExpense addExpense={addExpense}/>
+            <h2 style={{textAlign: 'center'}}>Expense Report</h2>
+            <NewExpense addExpense={addExpense} />
             <ExpensesContainer expenses={expenses} />
         </div>
     );
-}
+};
 
 export default App;
